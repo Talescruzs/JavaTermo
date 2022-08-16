8411787407203fc0cba7547812a37eee31baf04c
Talescruzs <tcs2017@hotmail.com>
2022-08-16 18:04:30 -0300

    Primeira tela (ainda não funciona)

---
+++ Controllers/SelectGameType.java
@@ -0,0 +1,33 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SelectGameType {
+
+    @FXML
+    private Button bt1;
+
+    @FXML
+    private Button bt2;
+
+    @FXML
+    private Button bt3;
+
+    @FXML
+    void StartHardcoreGame(ActionEvent event) {
+        System.out.println("aa");
+    }
+
+    @FXML
+    void StartNormalGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StartVersusGame(ActionEvent event) {
+
+    }
+
+}
--- Termo.java
+++ Termo.java
@@ -1,32 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Termo extends Application{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // System.out.println(getClass().getResource("Views/SelectGameType.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        FileInputStream fileInputStream = new FileInputStream(new File("Views/SelectGameType.fxml"));
+        Parent root = fxmlLoader.load(fileInputStream);
+        Scene view = new Scene(root);
+
         primaryStage.setTitle("Termo");
-        Button botao = new Button("Clica");
-
-        botao.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event){
-                System.out.println("Clicou");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().addAll(botao);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(view);
         primaryStage.show();
     }
 }
